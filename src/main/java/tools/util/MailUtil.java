6fe5923411452af1f48cf3613c655e52a6c19fbd
François Melkonian <francois.melkonian@gmail.com>
2018-11-13 21:18:35 +0100

    try with 2 mails

--- src/main/java/tools/util/MailUtil.java
+++ src/main/java/tools/util/MailUtil.java
@@ -11,27 +11,32 @@ import java.util.Properties;
 public class MailUtil {
 
     public static boolean sendEmail(String destination, String message){
-        Properties p = new Properties();
-        p.put("mail.debug",true);
-        Session session = Session.getDefaultInstance(p);
-        MimeMessage mimeMessage = new MimeMessage(session);
-        try {
-            mimeMessage.setText(message);
-            /*
-                todo: change to noreply@polyshare-cgjm.appspotmail.com
-                The email used in order to send email should be registered (Cloud console->parameters)
-             */
-            String fromEmail = "polyshare-cgjm@appspot.gserviceaccount.com";
+	    String fromEmail = "polyshare-cgjm@appspot.gserviceaccount.com";
 
-            mimeMessage.setFrom(new InternetAddress(fromEmail, "PolyShare"));
-            mimeMessage.addRecipients(Message.RecipientType.TO, destination);
-            mimeMessage.setSubject("Email test");
-            Transport.send(mimeMessage);
-            return true;
-        } catch (MessagingException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return false;
+	    return sendMsg(destination, message, fromEmail)&&  sendMsg(destination, message, "francois.melkonian@gmail.com");
 
     }
+
+	private static boolean sendMsg(String destination, String message, String fromEmail) {
+		Properties p = new Properties();
+		p.put("mail.debug",true);
+		Session session = Session.getDefaultInstance(p);
+		MimeMessage mimeMessage = new MimeMessage(session);
+		try {
+		    mimeMessage.setText(message);
+		    /*
+		        todo: change to noreply@polyshare-cgjm.appspotmail.com
+		        The email used in order to send email should be registered (Cloud console->parameters)
+		     */
+
+		    mimeMessage.setFrom(new InternetAddress(fromEmail, "PolyShare"));
+		    mimeMessage.addRecipients(Message.RecipientType.TO, destination);
+		    mimeMessage.setSubject("Email test");
+		    Transport.send(mimeMessage);
+		    return true;
+		} catch (MessagingException | UnsupportedEncodingException e) {
+		    e.printStackTrace();
+		}
+		return false;
+	}
 }
