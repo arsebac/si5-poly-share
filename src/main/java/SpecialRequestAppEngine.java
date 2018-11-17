9e2dec7a8ff1525dd94014772228bdf956d328de
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-11-17 16:01:17 +0100

    fix datastore helper usage

--- src/main/java/SpecialRequestAppEngine.java
+++ src/main/java/SpecialRequestAppEngine.java
@@ -14,15 +14,24 @@ import java.util.Random;
 
 @WebServlet(name = "Special Requests", value = "/special")
 public class SpecialRequestAppEngine extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        DatastoreHelper datastoreHelper = new DatastoreHelper();
+        this.getServletContext().setAttribute("datastoreHelper", datastoreHelper);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DatastoreHelper datastoreHelper = (DatastoreHelper) req.getServletContext().getAttribute("datastoreHelper");
+
         String mail = req.getParameter("mail");
         String size = req.getParameter("size");
         String title = req.getParameter("title");
         String url = new Date() + "-" + new Random().nextInt();
 
         try {
-            new DatastoreHelper().addVideo(mail, Long.getLong(size), url, title, false);
+            datastoreHelper.addVideo(mail, Long.getLong(size), url, title, false);
             resp.getWriter().print("The video " + title+" has been added");
         } catch (UserNotFoundException e) {
             resp.sendError(403,"The user " + mail + "cannot be found");
